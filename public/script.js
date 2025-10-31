fb60e7ee99eb202f95b8a1df8aaab18cd5329f43
LeVanToan-w <tl7378132@gmail.com>
2025-10-31 17:40:35 +0700

    update day

--- public/script.js
+++ public/script.js
@@ -2,7 +2,18 @@ function time() {
     const now = new Date();
     let h = now.getHours().toString().padStart(2, '0');
     let m = now.getMinutes().toString().padStart(2, '0');
-    document.getElementById('time').textContent = h + ':' + m;
+    let s = now.getSeconds().toString().padStart(2, '0');
+    document.getElementById('time').textContent = h + ':' + m  + ':' + s
 }
 setInterval(time, 1000);
 time();
+
+function date() {
+    const now = new Date();
+    let d = now.getDay();
+    let mo = now.getMonth();
+    let y = now.getUTCFullYear();
+    document.getElementById('date').textContent =  d + '/' + mo + '/' + y;
+}
+setInterval(date, 1000);
+date();
